bc90ce577749286b18695f961e354a57e8a4579d
Peter Cruckshank <petecruckshank@gmail.com>
2022-03-28 18:09:36 -0400

    ✨updated to now get and show random joke

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,8 +1,31 @@
+import { Link, json, useLoaderData } from "remix"
+import { db } from "~/utils/db.server"
+import type { LoaderFunction } from "remix"
+import type { Joke } from "@prisma/client"
+
+type LoaderData = { randomJoke: Joke }
+
+export const loader: LoaderFunction = async ({ params }) => {
+  // Get random joke from database
+  const count = await db.joke.count()
+  const randomRowNumber = Math.floor(Math.random() * count)
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  })
+  // check for joke, if not throw error
+  if (!randomJoke) throw new Error("Joke not found")
+  const data: LoaderData = { randomJoke }
+  return json(data)
+}
+
 export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>()
   return (
     <div>
-      <h1>What did the bee say to the gardener?</h1>
-      <p>Bee-quiet I'm trying to work over here!</p>
+      <h1>Here is your random joke:</h1>
+      <p>{data.randomJoke.content}</p>
+      <Link to=".">{data.randomJoke.name} Permalink</Link>
     </div>
   )
 }
