7e759435354ee1aa7e8bca39b14124a942b94df2
Peter Cruckshank <petecruckshank@gmail.com>
2022-04-10 18:22:05 -0400

    💄 Login/logout added to header UI

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,6 +1,7 @@
 import { Link, useLoaderData, json, Outlet } from "remix"
 import type { LinksFunction, LoaderFunction } from "remix"
 import { db } from "~/utils/db.server"
+import { getUser } from "~/utils/session.server"
 import stylesUrl from "~/styles/jokes.css"
 
 export const links: LinksFunction = () => {
@@ -8,16 +9,21 @@ export const links: LinksFunction = () => {
 }
 
 type LoaderData = {
+  user: Awaited<ReturnType<typeof getUser>>
   jokeListItems: Array<{ id: string; name: string }>
 }
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const jokeListItems = await db.joke.findMany({
+    take: 5,
+    select: { id: true, name: true },
+    orderBy: { createdAt: "desc" },
+  })
+  const user = await getUser(request)
+
   const data: LoaderData = {
-    jokeListItems: await db.joke.findMany({
-      take: 5,
-      select: { id: true, name: true },
-      orderBy: { createdAt: "desc" },
-    }),
+    jokeListItems,
+    user,
   }
   return json(data)
 }
@@ -34,6 +40,18 @@ export default function JokesRoute() {
               <span className="logo-medium">J🤪KES</span>
             </Link>
           </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
         </div>
       </header>
       <main className="jokes-main">
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,6 @@
 import type { ActionFunction, LinksFunction } from "remix"
 import { useActionData, json, Link, useSearchParams } from "remix"
-import { createUserSession, login } from "~/utils/session.server"
+import { createUserSession, login, register } from "~/utils/session.server"
 import { db } from "~/utils/db.server"
 import stylesUrl from "~/styles/login.css"
 
@@ -90,12 +90,14 @@ export const action: ActionFunction = async ({ request }) => {
           formError: `User with username ${username} already exists`,
         })
       }
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      })
+      const user = await register({ username, password })
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Something went wrong trying to create a new user.`,
+        })
+      }
+      return createUserSession(user.id, redirectTo)
     }
     default: {
       return badRequest({
