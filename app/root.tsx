3b11dd55002d295b6a79b8a673ff55a91d77ed8d
Peter Cruckshank <petecruckshank@gmail.com>
2022-05-23 18:41:58 -0400

    🎆 activated JS shiiping to the front end!

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,11 @@
-import { Links, LiveReload, Meta, Outlet, useCatch } from "@remix-run/react"
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  useCatch,
+} from "@remix-run/react"
 import type { LinksFunction, MetaFunction } from "@remix-run/node"
 
 import globalStylesUrl from "./styles/global.css"
@@ -55,6 +62,7 @@ function Document({
       </head>
       <body>
         {children}
+        <Scripts />
         <LiveReload />
       </body>
     </html>
@@ -84,6 +92,7 @@ export function CatchBoundary() {
 }
 
 export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error)
   return (
     <Document title="Uh-oh!">
       <div className="error-container">
